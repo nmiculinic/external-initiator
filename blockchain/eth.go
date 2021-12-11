fd4b64e4520b3f9bb92e868d916c0146cf4249ec
Neven Miculinic <neven.miculinic@gmail.com>
2021-12-11 19:47:05 +0100

    Various fixes while solving the problem

--- blockchain/eth.go
+++ blockchain/eth.go
@@ -32,14 +32,13 @@ func createEthManager(p subscriber.Type, config store.Subscription) ethManager {
 	}
 
 	var topics [][]common.Hash
-	var t []common.Hash
 	for _, value := range config.Ethereum.Topics {
 		if len(value) < 1 {
+			topics = append(topics, nil)
 			continue
 		}
-		t = append(t, common.HexToHash(value))
+		topics = append(topics, []common.Hash{common.HexToHash(value)})
 	}
-	topics = append(topics, t)
 
 	return ethManager{
 		fq: &filterQuery{
